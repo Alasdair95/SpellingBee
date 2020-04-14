797ceb0335753a82153e681b777300217d4e19d4
Alasdair <alasdairjohnpurves@gmail.com>
2020-04-14 21:03:11 +0100

    added more words

--- my_words.py
+++ my_words.py
@@ -1,3 +1,13 @@
-easy = ['cat', 'dog', 'feel', 'fast', 'slow', 'cup', 'fan']
-medium = ['little', 'people', 'thing', 'listen', 'mother']
-hard = ['hospital', 'remember', 'difficult', 'children', 'relationship']
+easy = ['cat', 'dog', 'feel', 'fast', 'slow', 'cup', 'fan', 'bark', 'tile', 'myth', 'zoo', 'fur', 'sour', 'sing',
+        'song', 'wee', 'zip', 'quiz', 'jar', 'ham', 'pony', 'lawn', 'loft', 'jump', 'bend', 'glow', 'hit', 'hard',
+        'hide', 'air', 'fox', 'rich', 'mask', 'shed', 'tune', 'urge', 'sock', 'shoe', 'pole', 'poet', 'mud', 'oven',
+        'frog', 'menu', 'bump', 'hat']
+medium = ['little', 'people', 'thing', 'listen', 'mother', 'bottle', 'nerve', 'alarm', 'mouse', 'travel', 'success',
+          'fruit', 'wrong', 'guess', 'family', 'house', 'money', 'tonight', 'happy', 'pretty', 'three', 'honey',
+          'excuse', 'start', 'friend', 'truth', 'school', 'person', 'heart', 'phone', 'reason', 'trust', 'crazy',
+          'party', 'sister', 'father', 'doctor', 'trouble', 'wedding', 'perfect', 'police', 'story', 'dinner',
+          'uncle', 'build']
+hard = ['hospital', 'remember', 'difficult', 'children', 'relationship', 'understand', 'platform', 'purchase',
+        'publicity', 'brother', 'business', 'beautiful', 'tomorrow', 'important', 'daughter', 'different', 'marriage',
+        'certainly', 'question', 'especially', 'situation', 'accident', 'necessary', 'terrible', 'interest', 'thousand',
+        'birthday', 'building', 'attention', 'yesterday', 'security', 'afternoon', 'apologise', 'breakfast']
